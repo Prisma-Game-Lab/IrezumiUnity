3c8a6e315572fa9ce1436c09fa79099f05e2685d
Jose Eduardo Guimaraes Soares <je_soares94@yahoo.com.br>
2016-08-01 18:55:17 -0300

    When God closes a door, we make a trigger to dash through one ourselves
    Created a door prefab (you just can't see it), that only opens when the player dashes through its trigger

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class DoorTrigger : MonoBehaviour
+    {
+
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                Player pscript = other.gameObject.GetComponent<Player>();
+                if (pscript.IsDashing)
+                {
+                    Destroy(transform.parent.gameObject);
+                }
+            }
+        }
+
+    }
+}
