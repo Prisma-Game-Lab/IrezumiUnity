826b356be18f9efd9b81ae59df736b629ae3e1e8
GiuDuncan <giuliaduncan@gmail.com>
2016-08-01 16:10:37 -0300

    Mouse desabilitado no menu e no level selector
    pq botoes sao mt chatos e desativar o mouse é mais facil

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,69 +1,71 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
-public class ButtonManager : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class ButtonManager : MonoBehaviour {
 
-    // Singleton
-    private static ButtonManager _instance;
+        // Singleton
+        private static ButtonManager _instance;
 
-    public static ButtonManager Instance { get { return _instance; } }
+        public static ButtonManager Instance { get { return _instance; } }
 
-    private void Awake()
-    {
-        if (_instance != null && _instance != this)
+        private void Awake()
         {
-            Destroy(this.gameObject);
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
         }
-        else
+
+
+        /// <summary>
+        /// Scene Button Callback. Loads the scene passed as parameter.
+        /// </summary>
+        /// <param name="scene">Name if the scene.</param>
+        public void SceneButtonCallback(string scene)
         {
-            _instance = this;
+            SceneManager.LoadScene(scene);
         }
-    }
 
-
-    /// <summary>
-    /// Scene Button Callback. Loads the scene passed as parameter.
-    /// </summary>
-    /// <param name="scene">Name if the scene.</param>
-    public void SceneButtonCallback(string scene)
-    {
-        SceneManager.LoadScene(scene);
-    }
-
-    /// <summary>
-    /// Restart Button Callback. Restart the scene.
-    /// </summary>
-    public void RestartButtonCallback()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-	/// <summary>
-	/// Next level button callback. Loads the next level.
-	/// </summary>
-    public void NextLevelButtonCallback()
-    {
-        int scene = (SceneManager.GetActiveScene().buildIndex + 1);
-        if (scene > SceneManager.sceneCountInBuildSettings -1 )
+        /// <summary>
+        /// Restart Button Callback. Restart the scene.
+        /// </summary>
+        public void RestartButtonCallback()
         {
-            scene = 2;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        SceneManager.LoadScene(scene);
-    }
+
+        /// <summary>
+        /// Next level button callback. Loads the next level.
+        /// </summary>
+        public void NextLevelButtonCallback()
+        {
+            int scene = (SceneManager.GetActiveScene().buildIndex + 1);
+            if (scene > SceneManager.sceneCountInBuildSettings -1 )
+            {
+                scene = 2;
+            }
+            SceneManager.LoadScene(scene);
+        }
 
 
-    /// <summary>
-    /// Quit Button Callback. Quits the application.
-    /// </summary>
-    public void QuitButtonCallback()
-    {
-        // Game running in the editor
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        // Game running outside editor
-        #else
+        /// <summary>
+        /// Quit Button Callback. Quits the application.
+        /// </summary>
+        public void QuitButtonCallback()
+        {
+            // Game running in the editor
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            // Game running outside editor
+#else
         Application.Quit();
         #endif
+        }
     }
 }
