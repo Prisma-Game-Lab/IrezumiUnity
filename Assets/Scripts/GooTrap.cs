147cc5ec861e7a9d07461a5fc3ad9d0007bdcbf0
Jose Eduardo Guimaraes Soares <je_soares94@yahoo.com.br>
2016-08-03 15:44:23 -0300

    A sticky situation
    made the goo trap on the game by creating a GooTrap script, also created a new scene called ZeScene so that I can test stuff without the risk of conflicts on the commit

---
+++ Assets/Scripts/GooTrap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class GooTrap : MonoBehaviour
+    {
+        Player pscript;
+        bool isOnTrap;
+
+        void DeactivateTrap()
+        {
+            isOnTrap = false;
+            pscript.MoveSpeed = pscript.MoveSpeed * 2;
+            
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+
+            if (other.gameObject.tag == "Player" && isOnTrap == false)
+            {
+                isOnTrap = true;
+                pscript = other.gameObject.GetComponent<Player>();
+                Invoke("DeactivateTrap", 1.5f);
+                pscript.MoveSpeed = pscript.MoveSpeed / 2;
+
+            }
+        }
+
+    }
+}
+
