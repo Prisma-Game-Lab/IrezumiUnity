b3fb29cbf2899e9153f1a474c8b000fca65b9329
Renan da Fonte <renandafonte@hotmail.com>
2016-09-16 13:09:54 -0300

    Falling Platform
    Created falling platform prefab and used it on TestScene

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+	public class FallingPlatform : PlatformController
+	{
+		private float _fallingSpeed;
+		private Transform _transform;
+
+		// Use this for initialization
+		public override void Awake ()
+		{
+			base.Awake ();
+			_transform = GetComponent<Transform> ();
+
+			LocalWaypoints = new Vector3[2];
+			LocalWaypoints [0] = new Vector3 (0, 0, 0);
+			LocalWaypoints [1] = new Vector3 (0, -10000, 0);
+
+			_fallingSpeed = Speed;
+			Speed = 0;
+		}
+
+		void OnTriggerEnter2D (Collider2D collider)
+		{
+			if (collider.name == "Player") {
+				Speed = _fallingSpeed;
+			}
+		}
+	}
+}
+
