9645809c142f69e4ece49d3c35d96403c61223a6
Nicolas Paes Leme <nickxp51@hotmail.com>
2016-09-16 16:15:29 -0300

    Spooky spikes
    -Added the spikes prefab, placeholder art and spikes script. The currently do 1000 Damage we can change it later.

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class Spike : MonoBehaviour
+    {
+        void Start()
+        { 
+
+        }
+        #region CollisionEnter
+        void OnCollisionEnter2D(Collision2D other)
+        {
+           
+            if (other.gameObject.tag == "Player")
+            {
+               // other.gameObject.SendMessage("Damage", 1000);
+               other.gameObject.GetComponent<Player>().Damage(1000);
+            }
+            /* Revisao Pietro
+            else if (other.gameObject.tag == "Trap")
+            {
+                Physics2D.IgnoreLayerCollision(0, 15);
+            }
+            Fim */
+        }
+
+        #endregion
+
+        void Update()
+        {
+
+        }
+    }
+}
