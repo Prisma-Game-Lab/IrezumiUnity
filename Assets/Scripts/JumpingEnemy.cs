554fa2f9ec13c4c4b008b2da274f981d96df2da7
GiuDuncan <giuliaduncan@gmail.com>
2016-11-24 20:33:58 -0200

    Jumping Enemy - quase pronto - giulia e rudá
    mecanica basica funcionando, talvez alguns bugs

---
+++ Assets/Scripts/JumpingEnemy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class JumpingEnemy : MonoBehaviour
+    {
+
+
+        public Direction Direcao;
+        public int JumpHeight;
+        public float JumpDuration;
+        public float Velocity;
+        private Vector2 _velocity;
+        private Vector3 _enemyPosition;
+        private Dictionary<Direction, Vector2> _directionalVector;
+        private bool _jumping;
+
+        // Use this for initialization
+        void Start () {
+            _directionalVector = new Dictionary<Direction, Vector2>
+            {
+                {Direction.Cima, Vector2.down},
+                {Direction.Baixo, Vector2.up },
+                {Direction.Direita,  Vector2.left},
+                {Direction.Esquerda, Vector2.right }
+
+            };
+            JumpHeight = 7;
+            JumpDuration = 2;
+            Velocity = 7;
+        }
+        
+        // Update is called once per frame
+        void Update ()
+        {
+            _enemyPosition = this.transform.position;
+            
+            var controller = gameObject.GetComponent<JumpingEnemyController>();
+
+            if(!_jumping)
+                CheckCollisions(controller);
+            
+            transform.Translate(_velocity * Time.deltaTime);
+
+        }
+
+        private void CheckCollisions(JumpingEnemyController controller)
+        {
+            if (Direcao == Direction.Cima || Direcao == Direction.Baixo)
+                //Se movimento for para cima ou para baixo checar por colisao dem Y
+            {
+                if (controller.HitVertical((int) _directionalVector[Direcao].y))
+                {
+                    _velocity = new Vector3(0, JumpHeight, 0);
+                    _jumping = true;
+                    Invoke("ChangeJumping", JumpDuration);
+                }
+                else
+                {
+                    _velocity = _directionalVector[Direcao]* Velocity;
+                }
+            }
+            else //Se movimento for para direita ou para esquerda checar por colisao dem X
+            {
+                if (controller.HitHorizontal((int) _directionalVector[Direcao].x))
+                {
+                    _velocity = new Vector3(JumpHeight,0 , 0);
+                    _jumping = true;
+                    Invoke("ChangeJumping", JumpDuration);
+                }
+                else
+                {
+                    _velocity = _directionalVector[Direcao]* Velocity;
+                }
+            }
+        }
+
+        void ChangeJumping()
+        {
+            _jumping = !_jumping;
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.gameObject.GetComponent<Player>().Damage(1000);
+            }
+        }
+
+        public enum Direction
+        {
+            Cima, Baixo, Direita, Esquerda
+        }
+    }
+}
---
+++ Assets/Scripts/JumpingEnemyController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class JumpingEnemyController : RaycastController {
+
+        // Use this for initialization
+        void Start () {
+	
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            UpdateRaycastOrigins();
+            if (this.tag == "DeadEnemy")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        public bool HitVertical(int directionY)
+        {
+            for (var i = 0; i < VerticalRayCount; i++)
+            {
+                var rayLength = SkinWidth;
+
+                Vector2 rayOrigin = (directionY == -1) ? RaycastOrigins.BottomLeft : RaycastOrigins.TopLeft;
+                rayOrigin += Vector2.right * (VerticalRaySpacing * i);
+                Debug.DrawRay(rayOrigin, Vector2.up * directionY , Color.green);
+
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, CollisionMask);
+                if (hit)
+                    return true;
+            }
+            return false;
+        }
+
+        public bool HitHorizontal(int directionX)
+        {
+            for (var i = 0; i < HorizontalRayCount; i++)
+            {
+                var rayLength = SkinWidth;
+
+                Vector2 rayOrigin = (directionX == -1) ? RaycastOrigins.BottomLeft : RaycastOrigins.BottomRight;
+                rayOrigin += Vector2.up * (HorizontalRaySpacing * i);
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX, Color.red);
+
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, CollisionMask);
+                if (hit)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
